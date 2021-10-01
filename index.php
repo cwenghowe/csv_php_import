fc66a8283240ac39de51940f075f206e417ea014
cwenghowe <49468232+cwenghowe@users.noreply.github.com>
2021-10-01 21:12:28 +0800

    minor updates

--- index.php
+++ index.php
@@ -3,13 +3,6 @@
 
     use PhpOffice\PhpSpreadsheet\Spreadsheet;
     use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-
-    $spreadsheet = new Spreadsheet();
-    $sheet = $spreadsheet->getActiveSheet();
-    $sheet->setCellValue('A1', 'Hello World !');
-
-    $writer = new Xlsx($spreadsheet);
-    $writer->save('hello world.xlsx');
 ?>
 
 <!DOCTYPE html>
