52bf2eef6465bc2bf6ed4de98fe54d5b1ca75df7
cwenghowe <49468232+cwenghowe@users.noreply.github.com>
2021-10-01 10:41:51 +0800

    added import from xlsx functionality

--- index.php
+++ index.php
@@ -47,12 +47,20 @@
             $open = "<tr>";
             $end  = "</tr>";
             $init  = "<td>".$i."</td>";
-            $form1 = "<input type='text' name='matric[]' value='".$data1."'></input>";
-            $form2 = "<input type='text' name='name[]' value='".$data2."' size='50'></input>";
-            $form3 = "<input type='text' name='email[]' value='".$data3."'></input>";
+            $form1 = "<input type='text' name='matric[]' value='".$data1."' size='50'></input>";
+            $form2 = "<input type='text' name='name[]' value='".$data2."' ></input>";
+            $form3 = "<input type='text' name='email[]' value='".$data3."' size='40'></input>";
             $form4 = "<input type='text' name='group[]' value='".$data4."'></input>";
             return print $open.$init."<td>".$form1."</td><td>".$form2."</td><td>".$form3."</td><td>".$form4."</td>".$end;
         }
+    }
+
+    function printFormHeader() {
+        return print "<form method='post' action=''><table border=1>";
+    }
+
+    function printFormFooter() {
+
     }
 
     if (isset($_POST['submit'])) 
@@ -67,13 +75,14 @@
             $handle = fopen($_FILES['filename']['tmp_name'], "r");
             $headers = fgetcsv($handle, 1000, ","); // remove header
             $student = 0;
-            echo "<form method='post' action=''><table border=1>";
+            printFormHeader();
             printNameListRow("NO.",$headers[0],$headers[1],$headers[2],$headers[3],TRUE);
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
             {
-            printNameListRow($student+1,$data[0],$data[1],$data[2],$data[3]);
-            $student++;
+                printNameListRow($student+1,$data[0],$data[1],$data[2],$data[3]);
+                $student++;
             }
+            
             
             echo "</table>";
             echo "Total students in the list: ".$student."<br>";
@@ -82,9 +91,43 @@
             fclose($handle);
         } 
         else if($ext="xlsx") {
-            $sheetReader = \PhpOffice\PhpSpreadsheet\IOFactory::createReaderForFile($_FILES['filename']['tmp_name']);
-            $sheetReader->setReadDataOnly(true);
-            $sheetReader->load($_FILES['filename']['tmp_name']);
+            $spreadsheet = new Spreadsheet();
+
+            $inputFileType = 'Xlsx';
+            $inputFileName = $_FILES['filename']['tmp_name'];
+
+            $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+            $reader->setReadDataOnly(true);
+
+            $worksheetData = $reader->listWorksheetInfo($inputFileName);
+
+            $data = array();
+            foreach ($worksheetData as $worksheet) {
+                $sheetName = $worksheet['worksheetName'];
+                $reader->setLoadSheetsOnly($sheetName);
+                $spreadsheet = $reader->load($inputFileName);
+
+                $worksheet = $spreadsheet->getActiveSheet();
+                $data = $worksheet->toArray();
+                // print_r($worksheet->toArray());
+            }
+            // print_r($data);
+            printFormHeader();
+            $student = -1;
+
+            foreach ($data as $row) {
+                if($student==-1) {
+                    printNameListRow("No.",$row[0],$row[1],$row[2],$row[3],true);
+                } else {
+                    printNameListRow($student+1,$row[0],$row[1],$row[2],$row[3]);
+                    
+                }
+                $student++;
+            }      
+            echo "</table>";
+            echo "Total students in the list: ".$student."<br>";
+            echo "<input type='submit' name='save' value='save'></input></form>";
+            echo "<br>";
         }
         else if($ext=="") {
             echo "To use this app, please upload student list in CSV format!";
